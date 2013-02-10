602288fd1d7dd541515e201d7d8197606d18df27
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2013-02-09 23:13:09 -0800

    updated tcp chat client

--- node/tcp_chat.js
+++ node/tcp_chat.js
@@ -1,12 +1,16 @@
 var net = require('net')
-var chatServer = net.createServer()
+var chatServer = net.createServer(),
+    clientList = []
 
 chatServer.on('connection', function(client) {
     client.write('Hi!\n');
+    clientList.push(client)
+
     client.on('data', function(data){
-      console.log(data)
+        for(var i = 0; i < clientList.length; i += 1) {
+          clientList[i].write(data)
+        }
     })
-    client.write('Bye!\n');
 })
 
 chatServer.listen(9000)
